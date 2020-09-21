9e1c6f72a08c712e691224d6b26ac851f422c998
Antonio Soto <asoto@solidq.com>
2020-09-21 16:21:36 +0200

    primer dia

--- recursos/Escalado.py
+++ recursos/Escalado.py
@@ -2,3 +2,12 @@ from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
+
+
+from sklearn.preprocessing import MinMaxScaler
+X_fruits_normalized = MinMaxScaler().fit(X_fruits).transform(X_fruits)
+
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
--- recursos/Matriz de Correlacion.py
+++ recursos/Matriz de Correlacion.py
@@ -1,1 +1,8 @@
 print(dta.corr())
+
+
+    correlacion = df_abandonos.corr() 
+    fig, ax = plt.subplots(figsize=(21, 21)) 
+    ax.matshow(correlacion) 
+    plt.xticks(range(len(correlacion.columns)), correlacion.columns); 
+    plt.yticks(range(len(correlacion.columns)), correlacion.columns); 
